188a53f87299ea0ed65720a872a620570079592c
chshin59 <chshin59@gmail.com>
2022-10-25 17:19:42 +0900

    init: 프로젝트 설정 및 리액트 추가

--- frontend/webpack/webpack.common.js
+++ frontend/webpack/webpack.common.js
@@ -5,8 +5,7 @@ const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = {
   entry: {
-    popup: path.join(srcDir, "popup.tsx"),
-    options: path.join(srcDir, "options.tsx"),
+    index: path.join(srcDir, "index.tsx"),
     background: path.join(srcDir, "background.ts"),
     content_script: path.join(srcDir, "content_script.tsx"),
   },
