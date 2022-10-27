984d5a88d498004c2033a80ec53576ae96fcf669
jimin <jimin4661@gmail.com>
2022-10-27 16:42:39 +0900

    feat: add mainboard frame(#6)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 // import Container from "react-bootstrap/Container"
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <p className="fs-1 ms-2">hello dashboard</p>
+      <Dashboard/>      
     </div>
   );
 }
