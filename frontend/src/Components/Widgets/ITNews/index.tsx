565a880d43f3eb025eab0fb6397f017bf70b20bb
김영훈 <redwhich@gmail.com>
2022-11-01 16:44:40 +0900

    init: fileSetting(#8)

---
+++ frontend/src/Components/Widgets/ITNews/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardComponent from '../../Common/CardComponent';
+import './ITNews.css';
+import '../Calendar/Calendar.css';
+
+export default function ITNews() {
+  return (
+    <div className="news board">
+      <div className="news-title"></div>
+      <div className="news-main "></div>
+    </div>
+  );
+}
