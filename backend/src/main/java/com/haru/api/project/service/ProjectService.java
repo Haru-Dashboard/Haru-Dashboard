52e844e0d025a45c5ce1f6ad99aa9a3ddd1de1b8
민상규 <qksl7504@naver.com>
2022-11-04 17:52:53 +0900

    feat: add project detail

--- backend/src/main/java/com/haru/api/project/service/ProjectService.java
+++ backend/src/main/java/com/haru/api/project/service/ProjectService.java
@@ -1,19 +1,20 @@
 package com.haru.api.project.service;
 
+import com.haru.api.common.exception.PermissionException;
 import com.haru.api.file.domain.S3FileRepository;
-import com.haru.api.file.dto.S3FileResponse;
 import com.haru.api.file.entity.S3File;
+import com.haru.api.file.service.S3FileService;
 import com.haru.api.file.service.S3Service;
 import com.haru.api.project.domain.entity.Project;
 import com.haru.api.project.domain.entity.ProjectLabel;
 import com.haru.api.project.domain.entity.ProjectLink;
-import com.haru.api.project.domain.repository.LabelRepository;
 import com.haru.api.project.domain.repository.ProjectLabelRepository;
 import com.haru.api.project.domain.repository.ProjectLinkRepository;
 import com.haru.api.project.domain.repository.ProjectRepository;
 import com.haru.api.project.dto.ProjectLinkRequest;
 import com.haru.api.project.dto.ProjectRequest;
 import com.haru.api.project.dto.ProjectResponse;
+import com.haru.api.project.exception.ProjectNotFoundException;
 import com.haru.api.user.domain.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,27 +34,52 @@ public class ProjectService {
     private final ProjectLabelRepository projectLabelRepository;
     private final S3FileRepository fileRepository;
     private final S3Service s3Service;
+    private final S3FileService s3FileService;
 
     @Transactional
     public ProjectResponse.OnlyId create(ProjectRequest.CreateOrUpdate request, MultipartFile file, User user) {
         Project project = Project.create(request, user);
-        if(!request.getLinks().isEmpty()) {
+        if (!request.getLinks().isEmpty()) {
             List<ProjectLinkRequest.Create> links = request.getLinks();
             List<ProjectLink> projectLinks = links.stream().map(link -> ProjectLink.create(link, project)).collect(Collectors.toList());
             projectLinkRepository.saveAll(projectLinks);
         }
-        if(!request.getLabels().isEmpty()) {
+        if (!request.getLabels().isEmpty()) {
             List<String> labels = request.getLabels();
             List<ProjectLabel> projectLabels = labels.stream().map(label -> ProjectLabel.create(label, project)).collect(Collectors.toList());
             projectLabelRepository.saveAll(projectLabels);
         }
         S3File savedImage;
-        if(!file.isEmpty()) {
-            savedImage =  fileRepository.save(S3File.create(s3Service.upload(file)));
+        if (!file.isEmpty()) {
+            savedImage = fileRepository.save(S3File.create(s3Service.upload(file)));
             project.updateS3File(savedImage);
         }
         Project savedProject = projectRepository.save(project);
         return ProjectResponse.OnlyId.build(savedProject);
     }
 
+    public ProjectResponse.GetProject getProject(Long projectId, User user) {
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+        if (!Objects.equals(user.getId(), project.getUser().getId())) throw new PermissionException();
+        return ProjectResponse.GetProject.build(project, s3FileService.getImage(project.getFile().getId()));
+    }
+
+    public List<ProjectResponse.GetProject> getProjectList(User user) {
+        List<Project> projects = projectRepository.findAllByUser(user);
+        return projects.stream().map(project -> ProjectResponse.GetProject.build(project, s3FileService.getImage(project.getFile().getId()))).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public ProjectResponse.OnlyId update(Long projectId, User user) {
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+        if (!Objects.equals(user.getId(), project.getUser().getId())) throw new PermissionException();
+        return null;
+    }
+
+    @Transactional
+    public ProjectResponse.OnlyId delete(Long projectId, User user) {
+        Project project = projectRepository.findById(projectId).orElseThrow(ProjectNotFoundException::new);
+        if (!Objects.equals(user.getId(), project.getUser().getId())) throw new PermissionException();
+        return null;
+    }
 }
