b9ea4f0981f5f4cc93a3a8cd1dfa29e00eb73ca6
민상규 <qksl7504@naver.com>
2022-11-06 00:55:02 +0900

    feat: add Category search

--- backend/src/main/java/com/haru/api/category/domain/repository/CategoryRepository.java
+++ backend/src/main/java/com/haru/api/category/domain/repository/CategoryRepository.java
@@ -4,7 +4,11 @@ import com.haru.api.category.domain.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByName(String name);
+
+    List<Category> findTop20ByNameContaining(String keyword);
 }
