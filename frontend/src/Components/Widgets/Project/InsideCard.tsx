4655f68182eaad860b8fe54f53b9db890fd42548
ssafyKim <redwhich@gmail.com>
2022-11-08 17:42:46 +0900

    feat:changeNamelt(#31)

--- frontend/src/Components/Widgets/Project/InsideCard.tsx
+++ frontend/src/Components/Widgets/Project/InsideCard.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 const InsideCard = (props: any) => {
-  const { lists } = props;
-  console.log(lists);
+  const { list } = props;
   return (
     <div className="h-100 p-2">
       <div className="w-90 h-40 bg-secondary mx-auto">
-        <img src={lists.imageInfo.imageUrl} className="w-100 h-100" />
+        <img src={list.imageInfo.imageUrl} className="w-100 h-100" />
       </div>
-      <div>{lists.title}</div>
+      <div>{list.title}</div>
       <div>
-        {console.log(lists.projectLabels)}
-        {lists.projectLabels[0].id}/////////{lists.projectLabels[0].name}
+        {list.projectLabels[0].id}/////////{list.projectLabels[0].name}
       </div>
-      <div>{lists.endDate} </div>
-      <div>{lists.content}</div>
+      <div>{list.endDate} </div>
+      <div>{list.content}</div>
     </div>
   );
 };
