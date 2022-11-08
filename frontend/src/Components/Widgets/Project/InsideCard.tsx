315f783b549d8f1a0181c14e256b95c6733fb8b8
ssafyKim <redwhich@gmail.com>
2022-11-08 13:38:44 +0900

    feat:Project CSS&communication(#31)

--- frontend/src/Components/Widgets/Project/InsideCard.tsx
+++ frontend/src/Components/Widgets/Project/InsideCard.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-const InsideCard = () => {
+const InsideCard = (props: any) => {
+  const { lists } = props;
+  console.log(lists);
   return (
     <div className="h-100 p-2">
-      <div className="w-90 h-40 bg-secondary mx-auto">image</div>
-      <div>title</div>
-      <div>tags</div>
-      <div>dates</div>
-      <div>links</div>
+      <div className="w-90 h-40 bg-secondary mx-auto">
+        <img src={lists.imageInfo.imageUrl} className="w-100 h-100" />
+      </div>
+      <div>{lists.title}</div>
+      <div>
+        {console.log(lists.projectLabels)}
+        {lists.projectLabels[0].id}/////////{lists.projectLabels[0].name}
+      </div>
+      <div>{lists.endDate} </div>
+      <div>{lists.content}</div>
     </div>
   );
 };
--- frontend/src/Components/Widgets/Project/index.tsx
+++ frontend/src/Components/Widgets/Project/index.tsx
@@ -1,24 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BigTitle from '../../Common/Title/BigTitle';
 import CardComponent from '../../Common/CardComponent';
 import BtnPlus from '../../Common/Button/BtnPlus';
 import CreateProjectModal from './CreateProjectModal';
 import ProjectMoreModal from './ProjectMoreModal';
 import InsideCard from './InsideCard';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 const Project = () => {
   // 카드 컴포넌트를 세번 출력시키기 위한 임시 리스트
-  const list = [1, 1, 1];
+  //
+  //
+  //
+  //
+  const [lists, setLists] = useState([]);
+  const [pageNo, setPageNo] = useState(0);
+
+  function callList(pNo: number) {
+    const accessToken = 'Bearer ' + localStorage.getItem('accessToken');
+    const backURL = process.env.REACT_APP_BACKURL;
+    const URLNext = 'projects?page=' + pNo + '&size=3';
+    if (accessToken != null) {
+      fetch(backURL + URLNext, {
+        method: 'GET',
+        headers: { Authorization: accessToken },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          const A = [];
+          if (data.length) {
+            setLists(data);
+          }
+        });
+    }
+  }
+  useEffect(() => {
+    callList(pageNo);
+  }, []);
+  //
+  //
+  //
+  //
+  //
   const [showCreate, setShowCreate] = useState(false);
   const [showMore, setShowMore] = useState(false);
-  const [projectList, setProjectList] = useState([{}]);
-
   const handleCloseCreate = () => setShowCreate(false);
   const handleShowCreate = () => setShowCreate(true);
-
   const handleCloseMore = () => setShowMore(false);
   const handleShowMore = () => setShowMore(true);
 
+  function toBack() {
+    if (pageNo > 4) {
+      setPageNo(pageNo - 3);
+    } else {
+      setPageNo(1);
+    }
+  }
+  function toNext() {
+    setPageNo(pageNo + 3);
+  }
   /* TODO: project 조회 fetch 함수
   - 조회한 배열을 projectList에 저장하고
   - 아래 DOM에 list를 projectList로 바꿔주세요
@@ -33,17 +73,21 @@ const Project = () => {
           <BtnPlus onClick={handleShowCreate} />
         </div>
         <div className="d-flex justify-content-between h-70 mt-1">
-          {list.map((item) => {
+          <button
+            onClick={toBack}
+            style={{ visibility: pageNo <= 4 ? 'hidden' : 'visible' }}></button>
+          {lists.map((item, mapNo) => {
             return (
-              <div onClick={handleShowMore} className="w-30 h-100">
+              <div onClick={handleShowMore} className="w-30 h-100" key={mapNo}>
                 <CardComponent
                   cardWidth="100%"
                   cardHeight="100%"
-                  cardContent={<InsideCard />}
+                  cardContent={<InsideCard lists={item} />}
                 />
               </div>
             );
           })}
+          <button onClick={toNext}></button>
         </div>
       </div>
       {/* project를 생성하는 모달 */}
