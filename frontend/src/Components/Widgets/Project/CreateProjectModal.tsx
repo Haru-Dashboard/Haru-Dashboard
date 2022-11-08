d173e219764ea44c051397cfc21914ff092f241d
민상규 <qksl7504@naver.com>
2022-11-08 17:23:28 +0900

    feat: add labels

--- frontend/src/Components/Widgets/Project/CreateProjectModal.tsx
+++ frontend/src/Components/Widgets/Project/CreateProjectModal.tsx
@@ -1,11 +1,25 @@
 import React, { useState } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import SmallTitle from '../../Common/Title/SmallTitle';
+import BtnPlus from '../../Common/Button/BtnPlus';
 
 const FILE_SIZE_MAX_LIMIT = 5 * 1024 * 1024;
+type link = {
+  name: string;
+  url: string;
+};
+type form = {
+  title: string;
+  content: string;
+  labels: Array<string>;
+  links: Array<link>;
+  startDate: Date;
+  endDate: Date;
+};
 
 const CreateProjectModal = ({ handleClose, show }: any) => {
   const [file, setFile] = useState<File>();
+  const [form, setForm] = useState<form>();
 
   // TODO: project 생성 fetch 함수
   const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +27,6 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
     const files = (target.files as FileList)[0];
     if (files === undefined) return;
 
-    // 파일 용량 체크
     if (files.size > FILE_SIZE_MAX_LIMIT) {
       target.value = '';
       alert('업로드 가능한 최대 용량은 5MB입니다. ');
@@ -22,15 +35,66 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
 
     setFile(files);
   };
-  const createProject: React.FormEventHandler = (e) => {
+
+  const [inputs, setInputs] = useState<form>({
+    title: '',
+    content: '',
+    labels: [],
+    links: [{ name: '', url: '' }],
+    startDate: new Date(),
+    endDate: new Date(),
+  });
+
+  const handleInputChange = (event: any) => {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+    console.log(target);
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const addLabel = (event: any) => {
+    event.preventDefault();
+    setInputs({ ...inputs, labels: [...inputs.labels, event.target.value] });
+    event.target.value = '';
+  };
+
+  const enterLabel = (event: any) => {
+    event.preventDefault();
+    if (event.key !== 'Enter') return;
+    addLabel(event);
+  };
+
+  const addLink = (event: any) => {
+    const newLink: link = {
+      name: '',
+      url: '',
+    };
+    setInputs({ ...inputs, links: [...inputs.links, newLink] });
+  };
+
+  const createProject: React.FormEventHandler = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    console.log(file);
-    // formData.append(
-    //   'form',
-    //   new Blob([JSON.stringify(e.target.form)], { type: 'application/json' }),
-    // );
-    // formData.append('file', e.target.files[0]);
+
+    formData.append(
+      'form',
+      new Blob([JSON.stringify(inputs)], { type: 'application/json' }),
+    );
+    if (file !== undefined) formData.append('file', file);
+
+    const accessToken = 'Bearer ' + localStorage.getItem('accessToken');
+    if (accessToken !== null) {
+      const url = process.env.REACT_APP_BACKURL;
+      console.log(inputs);
+      await fetch(url + `projects`, {
+        method: 'POST',
+        headers: {
+          Authorization: accessToken,
+        },
+        body: formData,
+      });
+    }
   };
 
   return (
@@ -39,7 +103,7 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
         <Modal.Header className="pb-1" closeButton>
           <SmallTitle title="In Progress" color="black" />
         </Modal.Header>
-        <Form onSubmit={createProject} encType="multipart/form-data">
+        <Form encType="multipart/form-data">
           <Modal.Body>
             {/* content */}
             {/* TODO: 이미지 파일 업로드 */}
@@ -56,7 +120,9 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
             <Form.Control
               type="text"
               placeholder="제목"
+              name="title"
               className="my-2 border"
+              onChange={handleInputChange}
             />
             <Form.Control
               as="textarea"
@@ -64,14 +130,24 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
               className="my-2"
               style={{ resize: 'none' }}
               placeholder="내용을 입력하세요"
+              name="content"
+              onChange={handleInputChange}
             />
             {/* TODO: 태그를 INPUT 창에 입력하면 P태그 부분에 태그 형식으로 뜨도록 */}
 
             <p>작성한 태그가 뜨는 곳</p>
+            {inputs.labels.map((label, idx) => (
+              <span className="px-2" key={idx}>
+                {label}
+              </span>
+            ))}
+            <span></span>
             <Form.Control
               type="text"
               placeholder="태그 추가하기"
               className="my-2 border"
+              name="labels"
+              onKeyUp={enterLabel}
             />
 
             <Form.Group className="d-flex justify-content-between">
@@ -79,51 +155,44 @@ const CreateProjectModal = ({ handleClose, show }: any) => {
                 type="date"
                 placeholder="시작일"
                 className="my-2 border"
+                name="startDate"
+                onChange={handleInputChange}
               />
               <p className="mb-0 p-3">~</p>
               <Form.Control
                 type="date"
                 placeholder="종료일"
                 className="my-2 border"
+                name="endDate"
+                onChange={handleInputChange}
               />
             </Form.Group>
-            <Form.Group className="d-flex justify-content-between">
-              <Form.Control
-                type="text"
-                placeholder="링크명"
-                className="my-2 me-2 border w-50"
-              />
-              <Form.Control
-                type="url"
-                placeholder="링크 url"
-                className="my-2 border"
-              />
-            </Form.Group>
-            <Form.Group className="d-flex justify-content-between">
-              <Form.Control
-                type="text"
-                placeholder="링크명"
-                className="my-2 me-2 border w-50"
-              />
-              <Form.Control
-                type="url"
-                placeholder="링크 url"
-                className="my-2 border"
-              />
-            </Form.Group>
+            {inputs.links.map((_, idx) => (
+              <Form.Group className="d-flex justify-content-between" key={idx}>
+                <Form.Control
+                  type="text"
+                  placeholder="링크명"
+                  className="my-2 me-2 border w-50"
+                  name={`links[${idx}].name`}
+                  onChange={handleInputChange}
+                />
+                <Form.Control
+                  type="url"
+                  placeholder="링크 url"
+                  className="my-2 border"
+                  name={`links[${idx}].url`}
+                  onChange={handleInputChange}
+                />
+              </Form.Group>
+            ))}
+            <BtnPlus onClick={addLink} />
           </Modal.Body>
-          <Modal.Footer>
-            {/* 
-        onClick 
-        1. localStorage에 저장
-        2. TodayList에 emit해서 추가; state에 추가해서 새로고침하면 state는 날아가고 localStorage 것만 로드되도록 ; filterBar 로직 참고
-        3. 모달 닫기
-       */}
-            <Button type="submit" variant="outline-primary" size="sm">
-              SAVE
-            </Button>
-          </Modal.Footer>
         </Form>
+        <Modal.Footer>
+          <Button onClick={createProject} variant="outline-primary" size="sm">
+            SAVE
+          </Button>
+        </Modal.Footer>
       </Modal>
     </div>
   );
