3050af9094387531d4fa0577ff8b97d09e243e77
minhaeun <haeunee6575@gmail.com>
2022-11-10 23:42:45 +0900

    fix: Mvc Config 수정 및 gitignore 설정 (#7)

--- backend/src/main/java/com/haru/api/config/SecurityConfig.java
+++ backend/src/main/java/com/haru/api/config/SecurityConfig.java
@@ -12,8 +12,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
--- backend/src/test/java/com/haru/api/MvcTest.java
+++ backend/src/test/java/com/haru/api/MvcTest.java
@@ -2,6 +2,12 @@ package com.haru.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haru.api.config.WebMvcConfig;
+import com.haru.api.user.domain.repository.UserRepository;
+import com.haru.api.user.security.oauth2.CookieAuthorizationRequestRepository;
+import com.haru.api.user.security.oauth2.CustomOAuth2UserService;
+import com.haru.api.user.security.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import com.haru.api.user.security.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import com.haru.api.user.security.token.JwtAccessDeniedHandler;
 import com.haru.api.user.security.token.JwtAuthenticationEntryPoint;
 import com.haru.api.user.security.token.JwtTokenProvider;
 import com.haru.api.user.security.userdetails.CustomUserDetailService;
@@ -9,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.test.web.servlet.MockMvc;
 
 @AutoConfigureRestDocs(uriScheme = "https", uriHost = "k7a204.p.ssafy.io", uriPort = 443)
@@ -27,5 +34,19 @@ public abstract class MvcTest {
     protected CustomUserDetailService customUserDetailService;
     @MockBean
     protected JwtAuthenticationEntryPoint jwtAuthEntryPoint;
+    @MockBean
+    protected CookieAuthorizationRequestRepository cookieAuthorizationRequestRepository;
+    @MockBean
+    protected CustomOAuth2UserService customOAuth2UserService;
+    @MockBean
+    protected OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    @MockBean
+    protected OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    @MockBean
+    protected JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    @MockBean
+    protected UserRepository userRepository;
+    @MockBean
+    protected ClientRegistrationRepository clientRegistrationRepository;
 }
 
