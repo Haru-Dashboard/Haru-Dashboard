aa08509f5e09f96f8366b654ea43fdc5a27ab2e8
ssafyKim <redwhich@gmail.com>
2022-11-11 16:41:58 +0900

    fix:errorFix(#23)
    fix errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,8 @@
 import React, { useEffect } from 'react';
 import Dashboard from './Components/Dashboard';
 import Basic from './Components/Widgets/Basic';
-import { Authentication } from './API/Authentication';
 
 function App() {
-  useEffect(() => {
-    Authentication();
-  }, []);
-
   return (
     <div className="App">
       <div
